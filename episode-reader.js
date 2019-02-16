e6fd7e297dc26da0e12b7fa1adc6096dbd4a2eac
Adam <zealan@gmail.com>
2019-02-16 01:37:41 -0500

    get methods callable from list

--- episode-reader.js
+++ episode-reader.js
@@ -1,5 +1,23 @@
 const fs = require("fs");
 
+function countInstances(string, substring) {
+	return string.split(substring).length - 1;
+}
+
+function getFirstName(name) {
+	return name.split(" ")[0];
+}
+
+function getTimesMentioned(data, name) {
+	return countInstances(data, name);
+}
+
+function getTimesSpoken(data, name) {
+	return countInstances(data, name + ":");
+}
+
+var methods = [getTimesMentioned, getTimesSpoken];
+
 var contestants = fs.readFileSync("contestants.json").toString();
 contestants = JSON.parse(contestants);
 
@@ -8,18 +26,18 @@ fs.readdir('data', function(error, items) {
         fs.readFile('data/' + items[i], 'utf-8', function(error, data) {
 			for (i in contestants) {
 				var name = getFirstName(contestants[i]["name"]);
-				var timesMentioned = countInstances(data, name);
-				var timesSpoke = countInstances(data, name + ":");
-				console.log(contestants[i]["name"] + " " + timesMentioned + " " + timesSpoke);
+				var stats = [];
+				for (j in methods) {
+					var method = methods[j];
+					var result = method.call(null, data, name);
+					stats.push(result);
+				}
+				var ret = name + " ";
+				for (j in stats) {
+					ret += stats[j] + " ";
+				}
+				console.log(ret);
 			}
 		});
     }
 });
-
-function countInstances(string, substring) {
-	return string.split(substring).length - 1;
-}
-
-function getFirstName(name) {
-	return name.split(" ")[0];
-}
