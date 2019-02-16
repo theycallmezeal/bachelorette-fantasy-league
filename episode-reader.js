b07acfa018345fe5f72a32893634751028dd0cf8
Adam <zealan@gmail.com>
2019-02-16 01:20:11 -0500

    get times mentioned and times spoken counts

--- episode-reader.js
+++ episode-reader.js
@@ -1,15 +1,25 @@
 const fs = require("fs");
 
-var contestants;
-
-fs.readdir('contestants.csv', function(error, items) {
-	
-});
+var contestants = fs.readFileSync("contestants.json").toString();
+contestants = JSON.parse(contestants);
 
 fs.readdir('data', function(error, items) {
     for (i in items) {
-        fs.readFile('data/' + items[i], 'utf-8', function(readError, data) {
-			console.log(data);
+        fs.readFile('data/' + items[i], 'utf-8', function(error, data) {
+			for (i in contestants) {
+				var name = getFirstName(contestants[i]["name"]);
+				var timesMentioned = countInstances(data, name);
+				var timesSpoke = countInstances(data, name + ":");
+				console.log(contestants[i]["name"] + " " + timesMentioned + " " + timesSpoke);
+			}
 		});
     }
 });
+
+function countInstances(string, substring) {
+	return string.split(substring).length - 1;
+}
+
+function getFirstName(name) {
+	return name.split(" ")[0];
+}
