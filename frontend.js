b010100127259a76baf414637555282b89a2bf0a
Adam <zealan@gmail.com>
2019-02-17 00:37:27 -0500

    add leaderboard

--- frontend.js
+++ frontend.js
@@ -1,24 +1,65 @@
 var bacheloretteData = [];
 
-var request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:1693/');
+function refresh() {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://localhost:1693/');
 
-request.onload = function() {
-	if (request.status >= 200 && request.status < 400) {
-		var data = JSON.parse(request.responseText);
-		for (i in data) {
-			bacheloretteData.push(data[i]);
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			var data = JSON.parse(request.responseText);
+			while (bacheloretteData.length > 0) {
+				bacheloretteData.pop();
+			}
+			for (i in data) {
+				bacheloretteData.push(data[i]);
+			}
 		}
 	}
+
+	request.send();
 }
 
-request.send();
+refresh();
+document.getElementById("refresh").onclick = refresh;
+
+class FantasyPlayer {
+	constructor(name, men) {
+		this.name = name;
+		this.men = men;
+		this.score = 0;
+	}
+}
 
 var app = new Vue({
 	el: "#app",
 	data: {
-		bacheloretteData: bacheloretteData
+		bacheloretteData: bacheloretteData,
+		fantasyPlayers: [
+			new FantasyPlayer("Krzysztof", ["Roberto", "John", "Kasey", "Craig", "Phil"])
+		]
 	},
+	
+	methods: {
+		addPlayer: function (name, men) {
+			if (name != "") {
+				fantasyPlayers.append(new FantasyPlayer(name, men));
+			}
+		},
+		
+		scoreFor: function (man) {
+			var score = 0;
+			for (episode in this.bacheloretteData) {
+				for (setOfStats in this.bacheloretteData[episode].stats) {
+					if (this.bacheloretteData[episode].stats[setOfStats].name == man) {
+						score += this.bacheloretteData[episode].stats[setOfStats].score;
+						break; // handle duplicate names... shhh
+					}
+				}
+			}
+			return score;
+		}
+	},
+	
 	computed: {
 		stats: function () {
 			var ret = [];
